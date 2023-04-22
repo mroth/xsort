be89fbb2673706a537432f8ed247c2fe9acc3d29
Matthew Rothenberg <mrothenberg@gmail.com>
2023-04-22 16:36:45 -0400

    test: import simple wrapper tests from stdlib

---
+++ bench_test.go
@@ -0,0 +1,75 @@
+package xsort_test
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+
+	"github.com/mroth/xsort"
+)
+
+const n = 10_000_000
+
+func BenchmarkSearchInts(b *testing.B) {
+	xs := make([]int, n)
+	for i := range xs {
+		xs[i] = i
+	}
+
+	b.Run("lib=xsort", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			xsort.SearchInts(xs, n-1)
+		}
+	})
+
+	if !testing.Short() {
+		b.Run("lib=std", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				sort.SearchInts(xs, n-1)
+			}
+		})
+	}
+}
+
+func BenchmarkSearchFloat64s(b *testing.B) {
+	xs := make([]float64, n)
+	for i := range xs {
+		xs[i] = float64(i)
+	}
+
+	b.Run("lib=xsort", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			xsort.SearchFloat64s(xs, n-1)
+		}
+	})
+
+	if !testing.Short() {
+		b.Run("lib=std", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				sort.SearchFloat64s(xs, n-1)
+			}
+		})
+	}
+}
+
+func BenchmarkSearchStrings(b *testing.B) {
+	xs := make([]string, n)
+	for i := range xs {
+		xs[i] = fmt.Sprint(i)
+	}
+	x := fmt.Sprint(n - 1)
+
+	b.Run("lib=xsort", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			xsort.SearchStrings(xs, x)
+		}
+	})
+
+	if !testing.Short() {
+		b.Run("lib=std", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				sort.SearchStrings(xs, x)
+			}
+		})
+	}
+}
--- search_test.go
+++ search_test.go
@@ -1,75 +1,27 @@
-package xsort_test
+package xsort
 
 import (
-	"fmt"
-	"sort"
 	"testing"
-
-	"github.com/mroth/xsort"
 )
 
-const n = 10_000_000
+var data = []int{0: -10, 1: -5, 2: 0, 3: 1, 4: 2, 5: 3, 6: 5, 7: 7, 8: 11, 9: 100, 10: 100, 11: 100, 12: 1000, 13: 10000}
+var fdata = []float64{0: -3.14, 1: 0, 2: 1, 3: 2, 4: 1000.7}
+var sdata = []string{0: "f", 1: "foo", 2: "foobar", 3: "x"}
 
-func BenchmarkSearchInts(b *testing.B) {
-	xs := make([]int, n)
-	for i := range xs {
-		xs[i] = i
-	}
-
-	b.Run("lib=xsort", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			xsort.SearchInts(xs, n-1)
-		}
-	})
-
-	if !testing.Short() {
-		b.Run("lib=std", func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				sort.SearchInts(xs, n-1)
-			}
-		})
-	}
+var wrappertests = []struct {
+	name   string
+	result int
+	i      int
+}{
+	{"SearchInts", SearchInts(data, 11), 8},
+	{"SearchFloat64s", SearchFloat64s(fdata, 2.1), 4},
+	{"SearchStrings", SearchStrings(sdata, ""), 0},
 }
 
-func BenchmarkSearchFloat64s(b *testing.B) {
-	xs := make([]float64, n)
-	for i := range xs {
-		xs[i] = float64(i)
-	}
-
-	b.Run("lib=xsort", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			xsort.SearchFloat64s(xs, n-1)
+func TestSearchWrappers(t *testing.T) {
+	for _, e := range wrappertests {
+		if e.result != e.i {
+			t.Errorf("%s: expected index %d; got %d", e.name, e.i, e.result)
 		}
-	})
-
-	if !testing.Short() {
-		b.Run("lib=std", func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				sort.SearchFloat64s(xs, n-1)
-			}
-		})
-	}
-}
-
-func BenchmarkSearchStrings(b *testing.B) {
-	xs := make([]string, n)
-	for i := range xs {
-		xs[i] = fmt.Sprint(i)
-	}
-	x := fmt.Sprint(n - 1)
-
-	b.Run("lib=xsort", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			xsort.SearchStrings(xs, x)
-		}
-	})
-
-	if !testing.Short() {
-		b.Run("lib=std", func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				sort.SearchStrings(xs, x)
-			}
-		})
 	}
 }
