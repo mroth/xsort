2723d8dfbf360487d7af2ebe96b15a58270705b3
Matthew Rothenberg <mrothenberg@gmail.com>
2023-05-02 10:26:29 -0400

    proof of concept: generics to remove duplicate code
    zero performance impact, but requires go1.18+, and simply removes some
    code duplication. since the code duplication was the original reason
    this module was rejected for being upstreamed into the standard library,
    it's possible that this may be a better way to go in the future,
    especially once constraints.Ordered makes its way into the standard lib.
    Of course, at that point, you'd probably want to make SearchComparable
    or the like an exported function, and I suspect that will end up in the
    standard library anyhow once the `ordered` debate gets figured out.
    
    So for now just stashing in a separate branch to preserve the code.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mroth/xsort
 
-go 1.13
+go 1.18
+
+require golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53
--- search_go118.go
+++ search_go118.go
@@ -1,5 +1,5 @@
-//go:build !go1.18
-// +build !go1.18
+//go:build go1.18
+// +build go1.18
 
 // Package xsort contains manually inlined versions of the "search wrappers"
 // in the standard sort library.
@@ -11,21 +11,14 @@
 // performance overhead.
 package xsort
 
+import "golang.org/x/exp/constraints"
+
 // SearchInts searches for x in a sorted slice of ints and returns the index
 // as specified by Search. The return value is the index to insert x if x is
 // not present (it could be len(a)).
 // The slice must be sorted in ascending order.
 func SearchInts(a []int, x int) int {
-	i, j := 0, len(a)
-	for i < j {
-		h := int(uint(i+j) >> 1) // avoid overflow when computing h
-		if a[h] < x {
-			i = h + 1
-		} else {
-			j = h
-		}
-	}
-	return i
+	return searchComparable(a, x)
 }
 
 // SearchFloat64s searches for x in a sorted slice of float64s and returns the index
@@ -33,16 +26,7 @@ func SearchInts(a []int, x int) int {
 // present (it could be len(a)).
 // The slice must be sorted in ascending order.
 func SearchFloat64s(a []float64, x float64) int {
-	i, j := 0, len(a)
-	for i < j {
-		h := int(uint(i+j) >> 1) // avoid overflow when computing h
-		if a[h] < x {
-			i = h + 1
-		} else {
-			j = h
-		}
-	}
-	return i
+	return searchComparable(a, x)
 }
 
 // SearchStrings searches for x in a sorted slice of strings and returns the index
@@ -50,6 +34,15 @@ func SearchFloat64s(a []float64, x float64) int {
 // present (it could be len(a)).
 // The slice must be sorted in ascending order.
 func SearchStrings(a []string, x string) int {
+	return searchComparable(a, x)
+}
+
+// searchComparable searches for x in a sorted slice of constraints.Ordered and
+// returns the index as specified by Search. The return value is the index to
+// insert x if x is not present (it could be len(a)).
+//
+// The slice must be sorted in ascending order.
+func searchComparable[T constraints.Ordered](a []T, x T) int {
 	i, j := 0, len(a)
 	for i < j {
 		h := int(uint(i+j) >> 1) // avoid overflow when computing h
